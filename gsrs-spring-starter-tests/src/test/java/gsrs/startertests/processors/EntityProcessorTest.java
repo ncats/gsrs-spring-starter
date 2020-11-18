1d2d2234a52250b386e1ca23107d89f8f6ae3dad
katzelda <daniel.katzel@nih.gov>
2020-11-18 17:21:43 -0500

    added more  entityProcessor tests

--- gsrs-spring-starter-tests/src/test/java/gsrs/startertests/processors/EntityProcessorTest.java
+++ gsrs-spring-starter-tests/src/test/java/gsrs/startertests/processors/EntityProcessorTest.java
@@ -15,19 +15,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.persistence.Entity;
-import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.ArrayList;
@@ -63,7 +58,7 @@ public class EntityProcessorTest {
     }
 
     @Autowired
-    private TestEntityManager repository;
+    private TestEntityManager entityManager;
 
     @Autowired
     private PrincipalRepository principalRepository;
@@ -77,26 +72,106 @@ public class EntityProcessorTest {
     ClearAuditorRule clearAuditorRule;
 
     @Test
-    public void prePersist(){
+    public void persist(){
         MyEntity sut = new MyEntity();
 
         sut.setFoo("myDomain");
 
-        repository.persistAndFlush(sut);
+        entityManager.persistAndFlush(sut);
 
-        assertEquals(Arrays.asList("myDomain"), list);
+        assertEquals(Arrays.asList("prePersist myDomain", "postPersist myDomain"), list);
+
+    }
+
+    @Test
+    public void postLoad(){
+        MyEntity sut = new MyEntity();
+
+        sut.setFoo("myDomain");
+
+        entityManager.persistAndFlush(sut);
+
+//        sut=null;
+        list.clear();
+        //for some reason the entityManager.find() isn't having postLoad called, but refresh is so we will use that
+        entityManager.refresh(sut);
+        assertEquals("myDomain", sut.getFoo());
+        assertEquals(Arrays.asList("postLoad myDomain"), list);
+
+    }
+
+    @Test
+    public void update(){
+        MyEntity sut = new MyEntity();
+
+        sut.setFoo("myDomain");
+
+        sut = entityManager.persistAndFlush(sut);
+
+        list.clear();
+        sut.setFoo("domain2");
+        MyEntity sut2 =  entityManager.persistAndFlush(sut);
+
+
+        assertEquals(Arrays.asList("preUpdate domain2", "postUpdate domain2"), list);
+
+    }
+
+    @Test
+    public void remove(){
+        MyEntity sut = new MyEntity();
+
+        sut.setFoo("myDomain");
+
+        sut = entityManager.persistAndFlush(sut);
+
+        list.clear();
+
+       entityManager.remove(sut);
+        assertEquals(Arrays.asList("preRemove myDomain"), list);
+        //postRemove isn't called until flush
+        entityManager.flush();
+
+        assertEquals(Arrays.asList("preRemove myDomain", "postRemove myDomain"), list);
 
     }
 
     @Component
     public static class MyEntityProcessor implements EntityProcessor<MyEntity> {
 
-        public MyEntityProcessor(){
-            System.out.println("IN CONSTRUCTOR");
-        }
         @Override
         public void prePersist(MyEntity obj) {
-            list.add(obj.getFoo());
+            list.add("prePersist " + obj.getFoo());
+        }
+
+        @Override
+        public void postPersist(MyEntity obj) throws FailProcessingException {
+            list.add("postPersist " + obj.getFoo());
+        }
+
+        @Override
+        public void preRemove(MyEntity obj) throws FailProcessingException {
+            list.add("preRemove " + obj.getFoo());
+        }
+
+        @Override
+        public void postRemove(MyEntity obj) throws FailProcessingException {
+            list.add("postRemove " + obj.getFoo());
+        }
+
+        @Override
+        public void preUpdate(MyEntity obj) throws FailProcessingException {
+            list.add("preUpdate " + obj.getFoo());
+        }
+
+        @Override
+        public void postUpdate(MyEntity obj) throws FailProcessingException {
+            list.add("postUpdate " + obj.getFoo());
+        }
+
+        @Override
+        public void postLoad(MyEntity obj) throws FailProcessingException {
+            list.add("postLoad " + obj.getFoo());
         }
 
         @Override
