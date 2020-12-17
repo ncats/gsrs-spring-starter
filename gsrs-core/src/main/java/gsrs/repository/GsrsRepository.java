56681b3e6f26e9e920c1ece4fc619ab3f7755d7a
katzelda <daniel.katzel@nih.gov>
2020-12-17 08:35:59 -0500

    split GsrsRepository and GsrsVersionedRepository to only add hibernate lock hints if versioned.  errored out without

--- gsrs-core/src/main/java/gsrs/repository/GsrsRepository.java
+++ gsrs-core/src/main/java/gsrs/repository/GsrsRepository.java
@@ -15,48 +15,9 @@ import java.util.Optional;
 @NoRepositoryBean
 public interface GsrsRepository<T, ID> extends JpaRepository<T, ID> {
 
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+
     default Optional<T> findByKey(EntityUtils.Key key){
         return findById((ID)key.getIdNative());
     }
 
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    List<T> findAll();
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    List<T> findAll(Sort sort);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    List<T> findAllById(Iterable<ID> iterable);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    T getOne(ID id);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    <S extends T> List<S> findAll(Example<S> example);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    <S extends T> List<S> findAll(Example<S> example, Sort sort);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    Page<T> findAll(Pageable pageable);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    Optional<T> findById(ID id);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    <S extends T> Optional<S> findOne(Example<S> example);
-
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    @Override
-    <S extends T> Page<S> findAll(Example<S> example, Pageable pageable);
 }
---
+++ gsrs-core/src/main/java/gsrs/repository/GsrsVersionedRepository.java
@@ -0,0 +1,63 @@
+package gsrs.repository;
+
+import ix.core.util.EntityUtils;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import javax.persistence.LockModeType;
+import java.util.List;
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface GsrsVersionedRepository<T, ID> extends GsrsRepository<T, ID> {
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    default Optional<T> findByKey(EntityUtils.Key key){
+        return findById((ID)key.getIdNative());
+    }
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    List<T> findAll();
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    List<T> findAll(Sort sort);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    List<T> findAllById(Iterable<ID> iterable);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    T getOne(ID id);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    <S extends T> List<S> findAll(Example<S> example);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    <S extends T> List<S> findAll(Example<S> example, Sort sort);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    Page<T> findAll(Pageable pageable);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    Optional<T> findById(ID id);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    <S extends T> Optional<S> findOne(Example<S> example);
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Override
+    <S extends T> Page<S> findAll(Example<S> example, Pageable pageable);
+}
