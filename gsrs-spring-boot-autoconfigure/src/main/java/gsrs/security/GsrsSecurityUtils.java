187a295b9aa2c2fde3f19fde77b7a32a1f725cba
katzelda <daniel.katzel@nih.gov>
2021-03-01 21:56:32 -0500

    added static method for role checking of current user and getting current user. needed for substance validation

---
+++ gsrs-spring-boot-autoconfigure/src/main/java/gsrs/security/GsrsSecurityUtils.java
@@ -0,0 +1,44 @@
+package gsrs.security;
+
+import ix.core.models.Role;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GsrsSecurityUtils {
+    /**
+     * Get the current logged in user Object.
+     * @return an Object which is {@code null} if the user is not logged in;
+     */
+    public static Object getCurrentUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth ==null){
+            return null;
+        }
+        return auth.getPrincipal();
+    }
+    public static boolean hasAnyRoles(Collection<Role> roles){
+        return hasAnyRoles((String[]) roles.stream().map(r -> r.name()).toArray(size-> new String[size]));
+    }
+    public static boolean hasAnyRoles(Role...roles){
+        return hasAnyRoles((String[]) Arrays.stream(roles).map(r -> r.name()).toArray(size-> new String[size]));
+    }
+    public static boolean hasAnyRoles(String...roles){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth ==null){
+            return false;
+        }
+        Set<String> set = Arrays.stream(roles).map(r-> "ROLE_"+r).collect(Collectors.toSet());
+        for(GrantedAuthority ga : auth.getAuthorities()){
+            if(set.contains(ga.getAuthority())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
