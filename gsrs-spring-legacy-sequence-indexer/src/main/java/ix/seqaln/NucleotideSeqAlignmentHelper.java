71360d5154c5cd09112ee3822ceaab0ce331d240
katzelda <daniel.katzel@nih.gov>
2021-03-29 14:38:13 -0400

    initial import of seqaln module - bumped version

---
+++ gsrs-spring-legacy-sequence-indexer/src/main/java/ix/seqaln/NucleotideSeqAlignmentHelper.java
@@ -0,0 +1,22 @@
+package ix.seqaln;
+
+import org.jcvi.jillion.align.NucleotideSubstitutionMatrices;
+import org.jcvi.jillion.align.pairwise.NucleotidePairwiseSequenceAlignment;
+import org.jcvi.jillion.align.pairwise.PairwiseAlignmentBuilder;
+import org.jcvi.jillion.core.residue.nt.Nucleotide;
+import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
+
+/**
+ * Created by katzelda on 11/7/18.
+ */
+public class NucleotideSeqAlignmentHelper implements SequenceAlignmentHelper<Nucleotide, NucleotideSequence>{
+    @Override
+    public NucleotideSequence toSequence(String seq) {
+        return NucleotideSequence.of(Nucleotide.cleanSequence(seq, "N"));
+    }
+
+    @Override
+    public PairwiseAlignmentBuilder<Nucleotide, NucleotideSequence, NucleotidePairwiseSequenceAlignment> createAlignmentBuilder(NucleotideSequence query, NucleotideSequence target) {
+        return PairwiseAlignmentBuilder.createNucleotideAlignmentBuilder(query, target, NucleotideSubstitutionMatrices.getIdentityMatrix());
+    }
+}
