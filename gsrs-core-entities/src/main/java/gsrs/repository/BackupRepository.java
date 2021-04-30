18e220120fdb18fd5fc9bfe9cdb97eb9371db736
katzelda <daniel.katzel@nih.gov>
2021-04-30 10:35:54 -0400

    added streamAll() method

--- gsrs-core-entities/src/main/java/gsrs/repository/BackupRepository.java
+++ gsrs-core-entities/src/main/java/gsrs/repository/BackupRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Repository
 public interface BackupRepository extends GsrsRepository<BackupEntity, Long> {
 
     Optional<BackupEntity> findByRefid(String refid);
-
+    @Query("select e from BackupEntity e")
+    Stream<BackupEntity> streamAll();
 }
