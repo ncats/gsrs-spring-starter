7aed3dc492dc65d1ba017f7b012f37542e007a1a
Daniel Katzel <daniel.katzel@nih.gov>
2021-05-22 16:15:26 -0400

    added missing class

---
+++ gsrs-scheduled-tasks/src/main/java/gsrs/scheduledTasks/SpringQrtzScheduler.java
@@ -0,0 +1,51 @@
+package gsrs.scheduledTasks;
+
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+@Configuration
+public class SpringQrtzScheduler {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    /*
+    @Bean
+    public Scheduler scheduler(Trigger trigger, JobDetail job, SchedulerFactoryBean factory)
+            throws SchedulerException {
+        Scheduler scheduler = factory.getScheduler();
+        scheduler.scheduleJob(job, trigger);
+        scheduler.start();
+        return scheduler;
+    }
+
+    @Bean
+    public SchedulerFactoryBean scheduler(Trigger trigger, JobDetail job) {
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setConfigLocation(new ClassPathResource("quartz.properties"));
+
+        schedulerFactory.setJobFactory(springBeanJobFactory());
+        schedulerFactory.setJobDetails(job);
+        schedulerFactory.setTriggers(trigger);
+        return schedulerFactory;
+    }
+
+     */
+
+    @Bean
+    public SpringBeanJobFactory springBeanJobFactory() {
+        AutoWiringSpringBeanJobFactory jobFactory = new AutoWiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+}
