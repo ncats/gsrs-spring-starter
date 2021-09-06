c2964f8205da61c15f504eb6808fcbb6ec1fa670
Daniel Katzel <daniel.katzel@nih.gov>
2021-09-05 22:12:04 -0400

    added javadoc

--- gsrs-core/src/main/java/gsrs/springUtils/AutowireHelper.java
+++ gsrs-core/src/main/java/gsrs/springUtils/AutowireHelper.java
@@ -19,21 +19,48 @@ public class AutowireHelper implements ApplicationContextAware {
         this.applicationContext = applicationContext;
     }
 
+    /**
+     * Use Spring's dependency injection to only set any fields
+     * in this bean that are annotated as  @{@link org.springframework.beans.factory.annotation.Autowire},
+     * this does NOT make any Proxy objects to handle things like Transactional.
+     *
+     * @param bean the bean to autowire- can not be null.
+     */
     public void autowire(Object bean){
-//        System.out.println("before autowire " + bean);
-        AutowireCapableBeanFactory autowireCapableBeanFactory = applicationContext.getAutowireCapableBeanFactory();
-        autowireCapableBeanFactory.autowireBean(bean);
-//        System.out.println("after autowire " + bean);
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(bean);
     }
+
+    /**
+     * Use Spring's AOP Proxy system to make a proxy of the given bean to support things
+     * like Transactional AND also inject any dependencies from fields
+     * that are annotated with @{@link org.springframework.beans.factory.annotation.Autowire}.
+     * This should be equivalent to creating a Spring Bean in a Configuration except the newly created
+     * proxy is not added to Spring's Application Context.
+     * @param bean the bean to autowire and Proxy; can not be null.
+     * @param <T> the type of the Object and is only used for compiler infercing to let you not
+     *           have to cast the returned proxy object's type.
+     * @return a new Proxy object.
+     * @throws NullPointerException if bean is null.
+     */
     public <T> T autowireAndProxy(T bean){
         return autowireAndProxy(bean, bean.getClass().getSimpleName());
     }
+    /**
+     * Use Spring's AOP Proxy system to make a proxy of the given bean to support things
+     * like Transactional AND also inject any dependencies from fields
+     * that are annotated with @{@link org.springframework.beans.factory.annotation.Autowire}.
+     * This should be equivalent to creating a Spring Bean in a Configuration except the newly created
+     * proxy is not added to Spring's Application Context.
+     * @param bean the bean to autowire and Proxy.
+     * @param beanName the name of the bean.
+     * @param <T> the type of the Object and is only used for compiler infercing to let you not
+     *           have to cast the returned proxy object's type.
+     * @return a new Proxy object.
+     */
     public <T> T autowireAndProxy(T bean, String beanName){
-//        System.out.println("before autowire " + bean);
         AutowireCapableBeanFactory autowireCapableBeanFactory = applicationContext.getAutowireCapableBeanFactory();
         autowireCapableBeanFactory.autowireBean(bean);
         return (T) autowireCapableBeanFactory.applyBeanPostProcessorsAfterInitialization(bean, beanName);
-//        System.out.println("after autowire " + bean);
     }
 
 
