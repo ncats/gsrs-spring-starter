44372529cedb5f2e2b9bad3e74faef61860c9623
Tyler Peryea <tylerperyea@gmail.com>
2021-09-27 18:44:21 -0400

    added default special fields prop

--- gsrs-core/src/main/java/gsrs/SpecialFieldsProperties.java
+++ gsrs-core/src/main/java/gsrs/SpecialFieldsProperties.java
@@ -1,23 +1,28 @@
 package gsrs;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("ix.core")
 public class SpecialFieldsProperties {
 
-    private List<Map<String, Object>> exactsearchfields;
+    private List<Map<String, Object>> exactsearchfields = new ArrayList<>();
 
     public List<Map<String, Object>> getExactsearchfields() {
+        
         return exactsearchfields;
     }
 
     public void setExactsearchfields(List<Map<String, Object>> exactsearchfields) {
-        this.exactsearchfields = exactsearchfields;
+        if(exactsearchfields!=null) {
+            this.exactsearchfields = exactsearchfields;
+        }
+       
     }
 
 
