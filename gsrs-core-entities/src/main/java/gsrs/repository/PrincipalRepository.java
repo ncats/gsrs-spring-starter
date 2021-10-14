fc440e01c65c5769686f6dad56caeb6d2d10a9c3
Daniel Katzel <daniel.katzel@nih.gov>
2021-10-14 10:40:39 -0400

    removed deprecated admin queries

--- gsrs-core-entities/src/main/java/gsrs/repository/PrincipalRepository.java
+++ gsrs-core-entities/src/main/java/gsrs/repository/PrincipalRepository.java
@@ -17,28 +17,6 @@ public interface PrincipalRepository extends GsrsRepository<Principal, Long> {
 
     Principal findDistinctByUsernameIgnoreCase(String username);
 
-    /**
-     * Get the list of admin usernames.
-     * @apiNote This should not be directly called by users.
-     * @param paging
-     * @return
-     * @see #getAdminName()
-     */
-    @Query("select username from Principal  where admin=true")
-    List<String> _getAdminNames(Pageable paging);
 
-    /**
-     * Get an admin name this will only return at most 1 admin username
-     * and is not guarenteed to always return the same one.
-     * @return empty if there are no admins in the database.
-     */
-    default Optional<String> getAdminName(){
-        List<String> names = _getAdminNames(PageRequest.of(0,1));
-        if(names.isEmpty()){
-            return Optional.empty();
-        }
-        //name[0] shouldn't be null... we can't have null usernames right?
-        return Optional.of(names.get(0));
-    }
 
 }
