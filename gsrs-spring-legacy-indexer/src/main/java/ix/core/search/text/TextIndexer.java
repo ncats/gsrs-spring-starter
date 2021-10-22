a37c848c111fe4bc1d976de070fc654e2f640e37
Tyler Peryea <tylerperyea@gmail.com>
2021-10-21 22:36:37 -0400

    added missing feature for hiding facets

--- gsrs-spring-legacy-indexer/src/main/java/ix/core/search/text/TextIndexer.java
+++ gsrs-spring-legacy-indexer/src/main/java/ix/core/search/text/TextIndexer.java
@@ -14,7 +14,6 @@ import gov.nih.ncats.common.io.IOUtil;
 import gov.nih.ncats.common.stream.StreamUtil;
 import gov.nih.ncats.common.util.CachedSupplier;
 import gov.nih.ncats.common.util.TimeUtil;
-
 import gsrs.indexer.IndexValueMakerFactory;
 import gsrs.legacy.GsrsSuggestResult;
 import gsrs.repository.GsrsRepository;
@@ -131,6 +130,9 @@ public class TextIndexer implements Closeable, ProcessListener {
 	private List<IndexListener> listeners = new ArrayList<>();
 
 	private Set<String> alreadySeenDuringReindexingMode;
+	
+
+    
     /**
      * DO NOT CALL UNLESS YOU KNOW WHAT YOU ARE DOING.
      * This is exposed for dependency injection from
@@ -1071,8 +1073,9 @@ public class TextIndexer implements Closeable, ProcessListener {
         return taxonWriter;
     }
 
-	private TextIndexer(IndexerServiceFactory indexerServiceFactory, IndexerService indexerService, TextIndexerConfig textIndexerConfig, IndexValueMakerFactory indexValueMakerFactory, Function<EntityWrapper, Boolean> deepKindFunction) {
-		// empty instance should only be used for
+    private TextIndexer(IndexerServiceFactory indexerServiceFactory, IndexerService indexerService, TextIndexerConfig textIndexerConfig, IndexValueMakerFactory indexValueMakerFactory, Function<EntityWrapper, Boolean> deepKindFunction) {
+        
+        // empty instance should only be used for
 		// facet subsearching so we only need to have
 		// a single thread...
         this.indexerServiceFactory = indexerServiceFactory;
@@ -1330,9 +1333,9 @@ public class TextIndexer implements Closeable, ProcessListener {
         private static final Pattern ROOT_CONTEXT_ADDER = Pattern
                 .compile("(\\b(?!" + ROOT + "|" + ENTITY_PREFIX +")[^ :]*_[^ :]*[:])");
         
-
         private static final Pattern QUOTES_AROUND_WORD_REMOVER = Pattern
-                .compile("\"([^\" ]*)\"");
+                                .compile("\"([^\" ]*)\"");
+
 
         public IxQueryParser(String def) {
             super(def, createIndexAnalyzer());
@@ -1718,15 +1721,24 @@ public class TextIndexer implements Closeable, ProcessListener {
 
 
 	public class BasicLuceneSearchProvider implements LuceneSearchProvider{
-		private Sort sorter;
-		private Filter filter;
-		private int max;
+	    private Sort sorter;
+	    private Filter filter;
+	    private int max;
+	    private boolean includeFacets = true;
 
-		public BasicLuceneSearchProvider(Sort sorter, Filter filter, int max){
-			this.sorter=sorter;
-			this.filter=filter;
-			this.max=max;
-		}
+//		public BasicLuceneSearchProvider(Sort sorter, Filter filter, int max){
+//			this.sorter=sorter;
+//			this.filter=filter;
+//			this.max=max;
+//		}
+		
+
+        public BasicLuceneSearchProvider(Sort sorter,Filter filter, int max, boolean includeFacets){
+            this.sorter=sorter;
+            this.filter=filter;
+            this.max=max;
+            this.includeFacets=includeFacets;
+        }
 
 		@Override
 		public DefaultLuceneSearchProviderResult search(IndexSearcher searcher, TaxonomyReader taxon, Query query, FacetsCollector facetCollector) throws IOException {
@@ -1735,12 +1747,14 @@ public class TextIndexer implements Closeable, ProcessListener {
 			//FacetsCollector.
 			//with sorter
 			if (sorter != null) {
-				hits = (FacetsCollector.search(searcher, query, filter, max, sorter, facetCollector));
+			    hits = (FacetsCollector.search(searcher, query, filter, max, sorter, facetCollector));
 			//without sorter
 			}else {
-				hits = (FacetsCollector.search(searcher, query, filter, max, facetCollector));
+			    hits = (FacetsCollector.search(searcher, query, filter, max, facetCollector));
 			}
-			facets = new FastTaxonomyFacetCounts(taxon, facetsConfig, facetCollector);
+			if(includeFacets) {
+			    facets = new FastTaxonomyFacetCounts(taxon, facetsConfig, facetCollector);
+			}
 			return new DefaultLuceneSearchProviderResult(hits,facets);
 		}
 
@@ -1777,11 +1791,13 @@ public class TextIndexer implements Closeable, ProcessListener {
 			 * range/dynamic facets?
 			 *
 			 */
-			if (!options.getLongRangeFacets().isEmpty()){
+			if (options.getIncludeFacets() && !options.getLongRangeFacets().isEmpty()){
 				FacetsCollector.search(searcher, ddq, filter, options.max(), facetCollector);
 			}
 
-			facets = swResult.facets;
+			if(options.getIncludeFacets()) {
+			    facets = swResult.facets;
+			}
 			hits = swResult.hits;
 			return new DefaultLuceneSearchProviderResult(hits,facets);
 		}
@@ -1910,7 +1926,7 @@ public class TextIndexer implements Closeable, ProcessListener {
 
 		//no specified facets (normal search)
 		if (options.getFacets().isEmpty()) {
-			lsp = new BasicLuceneSearchProvider(sorter, filter, options.max());
+			lsp = new BasicLuceneSearchProvider(sorter, filter, options.max(), options.getIncludeFacets());
 		} else {
 			DrillDownQuery ddq = new DrillDownQuery(facetsConfig, query);
 			List<Filter> nonStandardFacets = new ArrayList<Filter>();
@@ -1953,13 +1969,13 @@ public class TextIndexer implements Closeable, ProcessListener {
 
 			// drilldown
 			} else {
-				lsp = new BasicLuceneSearchProvider(sorter, filter, options.max());
+				lsp = new BasicLuceneSearchProvider(sorter, filter, options.max(), options.getIncludeFacets());
 			}
 		} // facets is empty
 
 
 		//Promote special matches
-		if(searchResult.getOptions().getKindInfo() !=null && gsrsRepository !=null){
+		if(searchResult.getOptions().getPromoteSpecialMatches() && searchResult.getOptions().getKindInfo() !=null && gsrsRepository !=null){
 		    //TODO katzelda October 2020 : don't support sponsored fields yet that's a Substance only thing
 		    //Special "promoted" match types
 			Set<String> specialExactMatchFields =  searchResult.getOptions()
@@ -2017,10 +2033,12 @@ public class TextIndexer implements Closeable, ProcessListener {
 		LuceneSearchProviderResult lspResult=lsp.search(searcher, taxon,qactual,facetCollector);
 		hits=lspResult.getTopDocs();
 
-		collectBasicFacets(lspResult.getFacets(), searchResult);
+		if(options.getIncludeFacets()) {
+		     collectBasicFacets(lspResult.getFacets(), searchResult);
+		     collectLongRangeFacets(facetCollector, searchResult);
+		}
 
 		
-		collectLongRangeFacets(facetCollector, searchResult);
 
 		if(options.getIncludeBreakdown() && textIndexerConfig.isFieldsuggest() && options.getKind()!=null){
 			EntityInfo<?> entityMeta= EntityUtils.getEntityInfoFor(options.getKind());
@@ -2068,7 +2086,7 @@ public class TextIndexer implements Closeable, ProcessListener {
 
 						f = new FieldCacheTermsFilter(FIELD_KIND, analyzers.toArray(new String[0]));
 					}
-					LuceneSearchProvider lsp2 = new BasicLuceneSearchProvider(null, f, options.max());
+					LuceneSearchProvider lsp2 = new BasicLuceneSearchProvider(null, f, options.max(),true);
 					LuceneSearchProviderResult res=lsp2.search(searcher, taxon,oq.k(),facetCollector2);
 					res.getFacets().getAllDims(DEFAULT_ANALYZER_MATCH_FIELD_LIMIT).forEach(fr->{
 						if(fr.dim.equals(TextIndexer.ANALYZER_FIELD)){
@@ -2584,8 +2602,10 @@ public class TextIndexer implements Closeable, ProcessListener {
 	// like that
 	public void markChange(){
 		lastModified.set(TimeUtil.getCurrentTimeMillis());
-		//TODO katzelda October 2020 :haven't implemented caching yet
-//		IxCache.markChange();
+		
+//		if(gsrscache!=null) {
+//		    gsrscache.markChange();
+//		}
 
 	}
 
