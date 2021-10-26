22db844b3740514e1b580e4a9dc5e84c02dd34ae
Tyler Peryea <tylerperyea@gmail.com>
2021-10-26 16:02:58 -0400

    only remove quotes on cases where there are no spaces, periods, hyphens, colons

--- gsrs-spring-legacy-indexer/src/test/java/ix/core/search/QueryParseTest.java
+++ gsrs-spring-legacy-indexer/src/test/java/ix/core/search/QueryParseTest.java
@@ -17,17 +17,40 @@ import ix.core.search.text.TextIndexer.IxQueryParser;
 
 public class QueryParseTest {
 
-    private static final Pattern QUOTES_AROUND_WORD = Pattern
-            .compile("\"([^\" ]*)\"");
+    private static final Pattern QUOTES_AROUND_WORD_REMOVER = Pattern
+            .compile("\"([^\" :.-]*)\"");
+    final static String[] BREAK_TOKENS = new String[] {
+            " ", ".", "-", ":"      
+          };
 
     @Test
     public void quotesAroundWordShouldBeRemoved() throws Exception{
         
-        assertEquals("*test*", QUOTES_AROUND_WORD.matcher("\"*test*\"").replaceAll("$1"));
-        assertEquals("\"*another test*\" *test*", QUOTES_AROUND_WORD.matcher("\"*another test*\" \"*test*\"").replaceAll("$1"));
-        assertEquals("*test3* *test*", QUOTES_AROUND_WORD.matcher("\"*test3*\" \"*test*\"").replaceAll("$1"));
-        assertEquals("simple", QUOTES_AROUND_WORD.matcher("\"simple\"").replaceAll("$1"));
-        assertEquals("\"multi word\"", QUOTES_AROUND_WORD.matcher("\"multi word\"").replaceAll("$1"));
+        assertEquals("*test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*test*\"").replaceAll("$1"));
+        assertEquals("\"*another test*\" *test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*another test*\" \"*test*\"").replaceAll("$1"));
+        assertEquals("*test3* *test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*test3*\" \"*test*\"").replaceAll("$1"));
+        assertEquals("simple", QUOTES_AROUND_WORD_REMOVER.matcher("\"simple\"").replaceAll("$1"));
+        assertEquals("\"multi word\"", QUOTES_AROUND_WORD_REMOVER.matcher("\"multi word\"").replaceAll("$1"));
+    }
+    
+
+    @Test
+    public void quotesAroundWordWithHyphenShouldNotBeRemoved() throws Exception{
+        
+        assertEquals("\"*another-test*\" *test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*another-test*\" \"*test*\"").replaceAll("$1"));
+        assertEquals("*test3* *test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*test3*\" \"*test*\"").replaceAll("$1"));
+        assertEquals("\"multi-word\"", QUOTES_AROUND_WORD_REMOVER.matcher("\"multi-word\"").replaceAll("$1"));
+        assertEquals("\"50-00-0\"", QUOTES_AROUND_WORD_REMOVER.matcher("\"50-00-0\"").replaceAll("$1"));
+    }
+    
+    @Test
+    public void quotesAroundCommonTokenizedWordCharactersRemain() throws Exception{
+        
+        
+        for(String t: BREAK_TOKENS) {
+            assertEquals("\"*another" + t + "test*\" *test*", QUOTES_AROUND_WORD_REMOVER.matcher("\"*another" + t +"test*\" \"*test*\"").replaceAll("$1"));
+            
+        }
     }
     
     @Test
@@ -95,6 +118,21 @@ public class QueryParseTest {
         
         assertTrue("Simple contains query wrapped in quotes should be wildcard query", q instanceof WildcardQuery);
         assertEquals("text:*rentuxima*", q.toString());
+    }
+    
+
+    @Test
+    public void confirmWildcardContainsQueryWithOneBreakCharAndQuotesParsedAsComplexPhraseQuery() throws Exception{
+        IxQueryParser iqp = new TextIndexer.IxQueryParser("text");
+
+        for(String t: BREAK_TOKENS) {
+            Query q = iqp.parse("text:\"*rentu" + t + "xima*\"");
+            
+            assertTrue("Complex phrase query should NOT be phrase query", !(q instanceof PhraseQuery));
+            assertTrue("Complex phrase query should contain *", q.toString().contains("*rentu" + t + "xima*"));
+            assertEquals("Complex phrase query ComplexPhraseQuery Object", "org.apache.lucene.queryparser.complexPhrase.ComplexPhraseQueryParser$ComplexPhraseQuery",q.getClass().getName());
+        }
+       
     }
     
 }
