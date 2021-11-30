e668c3766a84ae8700b55d041c6d1ba6aab419f2
Daniel Katzel <daniel.katzel@nih.gov>
2021-11-30 11:35:17 -0500

    @ParentReference now supports methods

--- gsrs-core/src/main/java/ix/core/models/ParentReference.java
+++ gsrs-core/src/main/java/ix/core/models/ParentReference.java
@@ -5,14 +5,24 @@ import java.lang.annotation.*;
 /**
  * A Marker Annotation to denote that a field
  * is referencing a Parent object.
- * This should be used to annotate fields that point to parents
+ * This should be used to annotate fields that point to their one parent
  * in Entity objects that also are passed around as JSON
  * so that we can correctly reconstruct to object tree
  * if the JSON lacks the parent reference.
+ *
+ * If annotated on a field, then the field object type should be appropatie for
+ * all possible values the JSON structure can have as a parent.
+ *
+ * If annotated on a method, then the method signature should return void and have one parameter
+ * that is a type that is appropirate for all possible values the JSON structure can have as a parent.
+ *
+ * If annotated on a field, then that is the parent field that is the
+ * parent/owner of this entity.  If annotated on a method, then that method
+ * can decide if any fields should be set based on the passed in owner object.
  */
 @Documented
 @Retention(value= RetentionPolicy.RUNTIME)
 @Inherited
-@Target(value={ElementType.FIELD})
+@Target(value={ElementType.FIELD, ElementType.METHOD})
 public @interface ParentReference {
 }
