b4a52f4aca64641271871c28ef32b1d2fb43f67e
Mitch Miller <mitch.miller@thinkscience.us>
2022-03-28 18:10:06 -0400

    Added 'resolve' method

--- gsrs-data-exchange/src/main/java/gsrs/dataExchange/model/MappingActionFactoryMetadata.java
+++ gsrs-data-exchange/src/main/java/gsrs/dataExchange/model/MappingActionFactoryMetadata.java
@@ -2,7 +2,9 @@ package gsrs.dataExchange.model;
 
 import lombok.Data;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class MappingActionFactoryMetadata {
@@ -17,5 +19,23 @@ public class MappingActionFactoryMetadata {
         this.label = builder.getLabel();
         this.parameterFields = builder.getParameterFields();
     }
+
+    public Map<String, Object> resolve(Map<String, Object> initialParams){
+        Map<String,Object> returnMap = new LinkedHashMap<>();
+        initialParams.forEach((k,v)->returnMap.put(k,v));
+        for(MappingParameter param: parameterFields){
+            if(!returnMap.containsKey(param.getFieldName())){
+                Object o = param.getDefaultValue();
+                if(o!=null){
+                    returnMap.put(param.getFieldName(),o);
+                }
+                if(param.isRequired()){
+                    throw new IllegalStateException("Required field:\"" +  param.getFieldName() + "\" is not present and has no default value");
+                }
+            }
+        }
+        return returnMap;
+    }
+
 
 }
