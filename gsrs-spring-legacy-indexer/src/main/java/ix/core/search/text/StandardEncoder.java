5109be8739129842cf771541a987cac75d0ad8c4
Tyler Peryea <tylerperyea@gmail.com>
2022-03-31 10:32:31 -0400

    Update StandardEncoder.java

--- gsrs-spring-legacy-indexer/src/main/java/ix/core/search/text/StandardEncoder.java
+++ gsrs-spring-legacy-indexer/src/main/java/ix/core/search/text/StandardEncoder.java
@@ -4,5 +4,13 @@ public interface StandardEncoder{
     public static String encode(String s) {
         return null;
     }
+
+    default StandardEncoder combine(StandardEncoder enc){
+        StandardEncoder _this=this;
+        return (s)->{
+            String e=_this.encode(s);
+            return enc.encode(e);
+        };
+    }
 }
 
