8917d9029287a90c68c1b356084023217c8bc434
epuzanov <egor.puzanov@bfarm.de>
2022-05-31 13:59:20 +0200

    Overide LOB Types registration

--- gsrs-core/src/main/java/gsrs/repository/sql/dialect/GSRSPostgreSQLDialectCustom.java
+++ gsrs-core/src/main/java/gsrs/repository/sql/dialect/GSRSPostgreSQLDialectCustom.java
@@ -1,6 +1,6 @@
 package gsrs.repository.sql.dialect;
 
-import org.hibernate.dialect.PostgreSQL9Dialect;
+import org.hibernate.dialect.PostgreSQLDialect;
 import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
@@ -8,14 +8,22 @@ import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 /*
 Designed by Tyler Peryea
 */
-public class GSRSPostgreSQLDialectCustom extends PostgreSQL9Dialect {   
-	@Override
+public class GSRSPostgreSQLDialectCustom extends PostgreSQLDialect {
+
+    public GSRSPostgreSQLDialectCustom() {
+        super();
+
+        registerColumnType(java.sql.Types.BLOB, "bytea");
+        registerColumnType(java.sql.Types.CLOB, "text");
+    }
+
+    @Override
     public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
-    if (sqlTypeDescriptor.getSqlType() == java.sql.Types.BLOB) {
-      return BinaryTypeDescriptor.INSTANCE;
-    }else if (sqlTypeDescriptor.getSqlType() == java.sql.Types.CLOB) {
-      return LongVarcharTypeDescriptor.INSTANCE;
-	}
-    return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
-  }
- }
+        if (sqlTypeDescriptor.getSqlType() == java.sql.Types.BLOB) {
+            return BinaryTypeDescriptor.INSTANCE;
+        }else if (sqlTypeDescriptor.getSqlType() == java.sql.Types.CLOB) {
+            return LongVarcharTypeDescriptor.INSTANCE;
+        }
+        return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
+    }
+}
