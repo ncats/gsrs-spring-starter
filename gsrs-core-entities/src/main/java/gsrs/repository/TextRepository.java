f976c35b7f6c010d5d78d1ec63b1fcd82790bdb9
Mitch Miller <mitch.miller@thinkscience.us>
2022-09-12 10:53:47 -0400

    Added a transaction to the save call

--- gsrs-core-entities/src/main/java/gsrs/repository/TextRepository.java
+++ gsrs-core-entities/src/main/java/gsrs/repository/TextRepository.java
@@ -2,9 +2,10 @@ package gsrs.repository;
 
 import ix.core.models.Text;
 import org.springframework.stereotype.Repository;
-import java.util.UUID;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface TextRepository extends GsrsRepository<Text, Long> {
 
 }
--- gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/AbstractExportSupportingGsrsEntityController.java
+++ gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/AbstractExportSupportingGsrsEntityController.java
@@ -5,28 +5,38 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import gsrs.repository.TextRepository;
 import gsrs.security.hasAdminRole;
 import ix.core.models.Text;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 import java.util.Objects;
 
+@Slf4j
 public abstract class AbstractExportSupportingGsrsEntityController<C extends AbstractExportSupportingGsrsEntityController, T, I>
         extends AbstractLegacyTextSearchGsrsEntityController<C, T, I>{
 
     @Autowired
     private TextRepository textRepository;
 
+    @Autowired
+    protected PlatformTransactionManager transactionManager;
+
+
     //ExporterFactoryConfig
-    @hasAdminRole
     @PostGsrsRestApiMapping("/export/setup")
     public ResponseEntity<Object> handleExportConfigSave(@RequestParam("exportConfig") String exportConfigJson,
-                                                         @RequestParam Map<String, String> queryParameters) throws Exception {
+                                                         @RequestParam Map<String, String> queryParameters) {
+        log.trace("starting in handleExportConfigSave");
         Objects.requireNonNull(queryParameters.get("configurationName"), "input parameters must include a configurationName!");
         Text textObject = new Text(queryParameters.get("configurationName"), exportConfigJson);
-        Text savedText= textRepository.saveAndFlush(textObject);
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        Text savedText = transactionTemplate.execute(t-> textRepository.saveAndFlush(textObject));
+        log.trace("completed save of Text");
 
         ObjectNode resultNode = JsonNodeFactory.instance.objectNode();
         resultNode.put("Newly created configuration", savedText.id);
