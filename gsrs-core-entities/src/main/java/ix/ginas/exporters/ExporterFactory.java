7915ad0ad69b11fd1db058d6012c63ec8221d081
Mitch Miller <mitch.miller@thinkscience.us>
2022-10-18 17:37:11 -0400

    Tweaks to exporter factory schema generation

--- gsrs-core-entities/src/main/java/ix/ginas/exporters/ExporterFactory.java
+++ gsrs-core-entities/src/main/java/ix/ginas/exporters/ExporterFactory.java
@@ -1,6 +1,5 @@
 package ix.ginas.exporters;
 
-
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -10,7 +9,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.*;
-import java.util.function.Function;
 
 /**
  * Factory interface for making an {@link Exporter}
@@ -87,78 +85,36 @@ public interface ExporterFactory<T> {
         return generateSchemaNode(this.getClass().getSimpleName(), JsonNodeFactory.instance.objectNode());
     }
 
-    default String generateSchema(String exporterName, ObjectNode schemaNode) {
-        StringBuilder returnBuilder = new StringBuilder();
-        returnBuilder.append("{\n");
-        returnBuilder.append("$schema: \"https://json-schema.org/draft/2020-12/schema\",\n");
-        returnBuilder.append("\"$id\": \"https://gsrs.ncats.nih.gov/#/export.scrubber.schema.json\",\n");
-        returnBuilder.append("\"title\": \"Scrubber Parameters\",\n");
-        returnBuilder.append("\"description\":\"");
-        returnBuilder.append(exporterName);
-        returnBuilder.append("\",\n");
-        returnBuilder.append("\"type\": \"object\",\n");
-        returnBuilder.append("\"properties\": {\n");
-        List<String> fieldNames = new ArrayList<>();
-        schemaNode.fieldNames().forEachRemaining(fieldNames::add);
-        for(int i=0; i<fieldNames.size(); i++) {
-            returnBuilder.append("{ \"");
-            returnBuilder.append(fieldNames.get(i));
-            returnBuilder.append("\": {\n");
-            returnBuilder.append(" \"type\": ");
-            returnBuilder.append(schemaNode.get(fieldNames.get(i)));
-            returnBuilder.append("\n  }\n");
-            returnBuilder.append(" }");
-            if(i<fieldNames.size()-1){
-                returnBuilder.append(",");
-            }
-            returnBuilder.append("\n");
-        }
-        //returnBuilder.append("");
-        returnBuilder.append("  }\n");
-        returnBuilder.append("}");
-        return returnBuilder.toString();
-    }
-
     default JsonNode generateSchemaNode(String exporterName, ObjectNode schemaNode) {
         ObjectNode outputNode = JsonNodeFactory.instance.objectNode();
         outputNode.put("$schema",  "https://json-schema.org/draft/2020-12/schema");
         outputNode.put("$id", "https://gsrs.ncats.nih.gov/#/export.scrubber.schema.json");
-        outputNode.put("title", "Scrubber Parameters");
+        outputNode.put("title", "Exporter Parameters");
         outputNode.put("description", exporterName);
         outputNode.put("type", "object");
 
-        ArrayNode propertiesNode = JsonNodeFactory.instance.arrayNode();
+        ObjectNode propertiesNode = JsonNodeFactory.instance.objectNode();
         List<String> fieldNames = new ArrayList<>();
         schemaNode.fieldNames().forEachRemaining(fn->{
             ObjectNode singlePropertyNode = JsonNodeFactory.instance.objectNode();
-            ObjectNode typeNode = JsonNodeFactory.instance.objectNode();
-            typeNode.set("type", schemaNode.get(fn));
-            singlePropertyNode.set(fn, typeNode);
-            propertiesNode.add(singlePropertyNode);
+            String rawType = schemaNode.get(fn).toString();
+            if(rawType.endsWith("\"")) rawType= rawType.substring(0, rawType.length()-1);
+            if(rawType.startsWith("\"")) rawType= rawType.substring(1);
+            //System.out.println("rawType: "+ rawType);
+            if( rawType.toUpperCase(Locale.ROOT).contains("STRING") && rawType.contains("[")){
+
+                rawType="array";
+                ObjectNode arrayTypeNode = JsonNodeFactory.instance.objectNode();
+                arrayTypeNode.put("type", "string");
+                singlePropertyNode.set("items", arrayTypeNode);
+            } else if(rawType.contains("Integer")) {
+                rawType="integer";
+            }
+            singlePropertyNode.put("type", rawType);
+            propertiesNode.set(fn, singlePropertyNode);
         });
         outputNode.set("properties", propertiesNode);
 
         return outputNode;
-/*
-        returnBuilder.append("\"properties\": {\n");
-
-        for(int i=0; i<fieldNames.size(); i++) {
-            returnBuilder.append("{ \"");
-            returnBuilder.append(fieldNames.get(i));
-            returnBuilder.append("\": {\n");
-            returnBuilder.append(" \"type\": ");
-            returnBuilder.append(schemaNode.get(fieldNames.get(i)));
-            returnBuilder.append("\n  }\n");
-            returnBuilder.append(" }");
-            if(i<fieldNames.size()-1){
-                returnBuilder.append(",");
-            }
-            returnBuilder.append("\n");
-        }
-        //returnBuilder.append("");
-        returnBuilder.append("  }\n");
-        returnBuilder.append("}");
-        return returnBuilder.toString();
-*/
     }
 }
