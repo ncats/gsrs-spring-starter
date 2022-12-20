f230be187b6d0d3cd35eb8175122880fb807e059
Mitch Miller <mitch.miller@thinkscience.us>
2022-12-20 08:46:36 -0500

    added default hasTrueValue method

--- gsrs-data-exchange/src/main/java/gsrs/dataexchange/model/ProcessingAction.java
+++ gsrs-data-exchange/src/main/java/gsrs/dataexchange/model/ProcessingAction.java
@@ -9,4 +9,14 @@ Something that the user does to a record in the holding area
 public interface ProcessingAction<T> {
 
     T process(T stagingAreaRecord, T additionalRecord, Map<String, Object> parameters, Consumer<String> log) throws Exception;
+
+    default boolean hasTrueValue(Map<String, Object> parameters, String parameterName) {
+        if (parameters.get(parameterName) == null) {
+            return false;
+        }
+        if (parameters.get(parameterName) instanceof Boolean) {
+            return (Boolean) parameters.get(parameterName);
+        }
+        return (parameters.get(parameterName).toString().equalsIgnoreCase("true"));
+    }
 }
