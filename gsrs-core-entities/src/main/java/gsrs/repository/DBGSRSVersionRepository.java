f3f7ebb1bc8cc4c5e2c938d1fa52afd18ff49659
Hu <hul5@nih.gov>
2023-03-26 07:11:35 -0400

    add version table

---
+++ gsrs-core-entities/src/main/java/gsrs/repository/DBGSRSVersionRepository.java
@@ -0,0 +1,7 @@
+package gsrs.repository;
+
+import ix.core.models.DBGSRSVersion;
+
+public interface DBGSRSVersionRepository extends GsrsRepository<DBGSRSVersion, Long>{	
+
+}
---
+++ gsrs-core-entities/src/main/java/ix/core/models/DBGSRSVersion.java
@@ -0,0 +1,47 @@
+package ix.core.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import gov.nih.ncats.common.util.TimeUtil;
+import ix.ginas.models.serialization.GsrsDateDeserializer;
+import ix.ginas.models.serialization.GsrsDateSerializer;
+
+@Entity
+@Table(name = "ix_core_DB_GSRS_version")
+@Indexable(indexed = false)
+public class DBGSRSVersion {
+	
+	@Id    
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "LONG_SEQ_ID")
+    public Long id;
+	
+	@Column(nullable=false)
+	String entity;
+	
+	@Column(nullable=false)
+	String version;
+	
+	@JsonSerialize(using = GsrsDateSerializer.class)
+    @JsonDeserialize(using = GsrsDateDeserializer.class)
+    public Date modified = TimeUtil.getCurrentDate();
+	
+	String hash;
+	
+	public DBGSRSVersion() {}
+	
+	public DBGSRSVersion(String entity, String version) {
+		this.entity = entity;
+		this.version = version;
+	}
+	
+}
