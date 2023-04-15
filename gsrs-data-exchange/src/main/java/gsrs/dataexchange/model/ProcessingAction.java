806b35c5b3d9e12b57d5b97c417ad5bdd307983d
Mitch Miller <mitch.miller@thinkscience.us>
2023-04-14 21:09:46 -0400

    Added a method to ProcessingAction that necessary for the ScrubProcessingAction recently added to Substances

--- gsrs-data-exchange/src/main/java/gsrs/dataexchange/model/ProcessingAction.java
+++ gsrs-data-exchange/src/main/java/gsrs/dataexchange/model/ProcessingAction.java
@@ -23,6 +23,15 @@ public interface ProcessingAction<T> {
         return (parameters.get(parameterName).toString().equalsIgnoreCase("true"));
     }
 
+    default boolean hasStringValue(Map<String, Object> parameters, String parameterName) {
+        if (parameters.get(parameterName) == null) {
+            return false;
+        }
+        if (parameters.get(parameterName) instanceof String) {
+            return ((String) parameters.get(parameterName)).length()>0;
+        }
+        return (parameters.get(parameterName).toString().equalsIgnoreCase("true"));
+    }
     String getActionName();
 
     default List<String> getOptions(){
