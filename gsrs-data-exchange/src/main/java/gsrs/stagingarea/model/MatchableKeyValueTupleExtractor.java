6543c3fb4b8b51250a316e0fc64e7203bb9b3afd
Mitch Miller <mitch.miller@thinkscience.us>
2023-05-29 22:42:17 -0400

    Get matchable calculation specifics (which classes handle the calculations) and which parameters are used from the system configuration - at runtime

--- gsrs-data-exchange/src/main/java/gsrs/stagingarea/model/MatchableKeyValueTupleExtractor.java
+++ gsrs-data-exchange/src/main/java/gsrs/stagingarea/model/MatchableKeyValueTupleExtractor.java
@@ -1,8 +1,11 @@
 package gsrs.stagingarea.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.util.function.Consumer;
 
 public interface MatchableKeyValueTupleExtractor<T> {
+
     void extract(T t, Consumer<gsrs.stagingarea.model.MatchableKeyValueTuple> c);
 
     default MatchableKeyValueTupleExtractor<T> combine(MatchableKeyValueTupleExtractor<T> matchableKeyValueTupleExtractor) {
