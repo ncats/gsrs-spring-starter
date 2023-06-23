8c2671ac74f0a2a14ceff6795b6b37fd6b86b750
Hu <hul5@nih.gov>
2023-06-23 14:05:49 -0400

    modify version table query

--- gsrs-core-entities/src/main/java/gsrs/repository/DBGSRSVersionRepository.java
+++ gsrs-core-entities/src/main/java/gsrs/repository/DBGSRSVersionRepository.java
@@ -1,9 +1,11 @@
 package gsrs.repository;
 
+import org.springframework.data.jpa.repository.Query;
+
 import ix.core.models.DBGSRSVersion;
 
 public interface DBGSRSVersionRepository extends GsrsRepository<DBGSRSVersion, Long>{
-
-	DBGSRSVersion findTopByOrderByModifiedDesc();	
-
+	
+	@Query("select max(versionInfo) from DBGSRSVersion where entity = ?1")
+	String findMaxVersions(String entity);
 }
