cbb68e5c1db9fe772e2e844a5c216c06575bf8a0
Tyler Peryea <tylerperyea@gmail.com>
2023-10-13 14:15:23 -0400

    Update EntityContextLookup.java

--- gsrs-spring-boot-autoconfigure/src/main/java/gsrs/config/EntityContextLookup.java
+++ gsrs-spring-boot-autoconfigure/src/main/java/gsrs/config/EntityContextLookup.java
@@ -2,6 +2,9 @@ package gsrs.config;
 
 public class EntityContextLookup {
 
+    // TODO: This should eventually use the registry of services to make a 
+    // map rather than doing this hack, but it works for the cases that
+    // are currently being used.
     public static String getContextFromEntityClass(String className) {
         String contextNameToUse =className;
         if(contextNameToUse.contains(".")) {
