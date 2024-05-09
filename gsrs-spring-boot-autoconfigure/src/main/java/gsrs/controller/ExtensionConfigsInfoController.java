3b84492bc5edfe6d3e2df50b2e3f1d76ffdaf711
alx652 <welscha2@nih.gov>
2024-05-09 17:30:15 -0400

    improve endpoints

--- gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/ExtensionConfigsInfoController.java
+++ gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/ExtensionConfigsInfoController.java
@@ -19,20 +19,26 @@ import ix.core.util.pojopointer.LambdaParseRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-public class ExtensionConfigurationController {
+public class ExtensionConfigsInfoController {
 
     // These endpoints provide config objects that have been transformed from mapped
-    // properties. So this represents the configuration that were supplied to GSRS code.
+    // properties. So this represents the modified configuration that was supplied to GSRS code.
     // If everything goes smoothly after the final list was established, these should become
     // active. However, this data often depends on an initialization or cached supplier mechanism
     // that only kicks in under certain circumstances.  Sometime this is on application start;
     // other times it is a specific action. Therefore, you may get a null value or a blank response.
 
-    @Autowired
+    // context (entity) is not always used but is needed for Gateway routing
+
+    // Consider changing the endpoints to use service-info/api/v1/{service}/@xyzConfigs?entity=entity1
+
+        @Autowired
     private TextIndexerFactory textIndexerFactory;
 
     @Autowired
@@ -54,17 +60,17 @@ public class ExtensionConfigurationController {
     GsrsExportConfiguration gsrsExportConfiguration;
 
     @hasAdminRole
-    @GetMapping("/api/v1/@validatorConfigs")
-    public JsonNode getFinishedValidatorConfigs() {
+    @GetMapping("/api/v1/{context}/@validatorConfigs")
+    public JsonNode getFinishedValidatorConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
-        List<? extends ValidatorConfig> list = gsrsFactoryConfiguration.getValidatorConfigByContext("substances");
+        List<? extends ValidatorConfig> list = gsrsFactoryConfiguration.getValidatorConfigByContext(context);
         JsonNode node = mapper.valueToTree(list);
         return node;
     }
 
     @hasAdminRole
-    @GetMapping("/api/v1/@entityProcessorConfigs")
-    public JsonNode getFinishedVEntityPorcessorConfigs() {
+    @GetMapping("/api/v1/{context}/@entityProcessorConfigs")
+    public JsonNode getFinishedVEntityProcessorConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
         List<? extends EntityProcessorConfig> list = gsrsFactoryConfiguration.getEntityProcessors();
         JsonNode node = mapper.valueToTree(list);
@@ -72,17 +78,17 @@ public class ExtensionConfigurationController {
     }
 
     @hasAdminRole
-    @GetMapping("/api/v1/@importAdapterFactoryConfigs")
-    public JsonNode getImportAdapterFactoryConfigs() {
+    @GetMapping("/api/v1/{context}/@importAdapterFactoryConfigs")
+    public JsonNode getImportAdapterFactoryConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
-        List<? extends ImportAdapterFactoryConfig> list = gsrsFactoryConfiguration.getImportAdapterFactories("substances");
+        List<? extends ImportAdapterFactoryConfig> list = gsrsFactoryConfiguration.getImportAdapterFactories(context);
         JsonNode node = mapper.valueToTree(list);
         return node;
     }
 
     @hasAdminRole
     @GetMapping("/api/v1/{context}/@exporterFactoryConfigs")
-    public JsonNode getExporterFactoryConfigs() {
+    public JsonNode getExporterFactoryConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, List<? extends ExporterFactoryConfig>> mapList = gsrsExportConfiguration.reportConfigs();
         JsonNode node;
@@ -95,17 +101,17 @@ public class ExtensionConfigurationController {
     }
 
     @hasAdminRole
-    @GetMapping("/api/v1/@matchableCalculationConfigs")
-    public JsonNode getMatchableCalculationConfigs() {
+    @GetMapping("/api/v1/{context}/@matchableCalculationConfigs")
+    public JsonNode getMatchableCalculationConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
-        List<? extends MatchableCalculationConfig> list = gsrsFactoryConfiguration.getMatchableCalculationConfig("substances");
+        List<? extends MatchableCalculationConfig> list = gsrsFactoryConfiguration.getMatchableCalculationConfig(context);
         JsonNode node = mapper.valueToTree(list);
         return node;
     }
 
     @hasAdminRole
-    @GetMapping("/api/v1/@scheduledTaskConfigs")
-    public JsonNode getScheduledTaskConfigs() {
+    @GetMapping("/api/v1/{context}/@scheduledTaskConfigs")
+    public JsonNode getScheduledTaskConfigs(@PathParam("context") String context) {
         ObjectMapper mapper = new ObjectMapper();
         List<? extends GsrsSchedulerTaskPropertiesConfiguration.ScheduledTaskConfig> list = gsrsSchedulerTaskPropertiesConfiguration.getConfigs();
         JsonNode node = mapper.valueToTree(list);
@@ -113,8 +119,8 @@ public class ExtensionConfigurationController {
     }
 
     @hasAdminRole
-    @GetMapping("/api/v1/@registeredFunctionConfigs")
-    public JsonNode getRegisteredFunctionConfigs() {
+    @GetMapping("/api/v1/{context}/@registeredFunctionConfigs")
+    public JsonNode getRegisteredFunctionConfigs(@PathParam("context") String context) {
         // If no response, try viewing a chemical substance detail display in the UI.
         ObjectMapper mapper = new ObjectMapper();
         List<? extends RegisteredFunctionConfig> list = lambdaParseRegistry.reportConfigs();
@@ -124,7 +130,7 @@ public class ExtensionConfigurationController {
 
 //  Not sure how to do this
 //    @GetMapping("/api/v1/@indexValueMakerConfigs")
-//    public String getI() {
+//    public String getIndexValueMakerConfigs() {
 //        ObjectMapper mapper = new ObjectMapper();
 //        TextIndexer defaultInstance = textIndexerFactory.getDefaultInstance();
 //
