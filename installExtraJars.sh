44b20d7221a7cd99f698ff7092ead993ba2a837c
Mitch Miller <mitch.miller@thinkscience.us>
2024-06-04 08:46:42 -0400

    making latest structure-indexer available

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,1 +1,2 @@
 # Don't delete this file even if empty
+mvn install:install-file -Dfile=extraJars/structure-indexer-0.0.16.jar -DgroupId=gov.nih.ncats -DartifactId=structure-indexer -Dversion=0.0.16 -Dpackaging=jar
