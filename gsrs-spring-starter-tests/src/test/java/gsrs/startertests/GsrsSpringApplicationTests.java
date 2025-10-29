48f8946103a6006552a49bde340b9ce67f26613b
alx652 <welscha2@nih.gov>
2025-10-29 15:23:59 -0400

    clean-up

--- gsrs-spring-starter-tests/src/test/java/gsrs/startertests/GsrsSpringApplicationTests.java
+++ gsrs-spring-starter-tests/src/test/java/gsrs/startertests/GsrsSpringApplicationTests.java
@@ -1,14 +1,9 @@
 package gsrs.startertests;
 
 import gsrs.controller.GsrsControllerConfiguration;
-import ix.core.search.text.TextIndexerFactory;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-
 import gsrs.startertests.jupiter.AbstractGsrsJpaEntityJunit5Test;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
