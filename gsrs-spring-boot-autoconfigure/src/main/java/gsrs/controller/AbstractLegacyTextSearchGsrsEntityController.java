bbdf09728be8b29679ecdf896fdc858c2333f503
Mitch Miller <mitch.miller@thinkscience.us>
2026-09-01 14:40:29 -0400

    update method signature

--- gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/AbstractLegacyTextSearchGsrsEntityController.java
+++ gsrs-spring-boot-autoconfigure/src/main/java/gsrs/controller/AbstractLegacyTextSearchGsrsEntityController.java
@@ -31,6 +31,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -1294,7 +1295,7 @@ GET     /suggest       ix.core.controllers.search.SearchFactory.suggest(q: Strin
     
     
     @PreAuthorize("isAuthenticated()")
-    @PutGsrsRestApiMapping(value="/@userList/currentUser/etag/{etagId}") 
+    @PutGsrsRestApiMapping(value="/@userList/currentUser/etag/{etagId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> addToCurrentUserSavedListWithEtag( 
     		@RequestParam(value="listName",required=true) String listName,
 			   @PathVariable("etagId") String etagId,
